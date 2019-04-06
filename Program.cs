32c98c955860581c6b657bfcfc2935c07ee949c4
CHAABANI Anis <chaabanianis@MacBook-Pro-de-CHAABANI.local>
2019-04-06 19:29:09 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Solution1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.Width = 5;
+            rectangle.Height = 4;
+            Console.WriteLine("Rectangle Area = " + rectangle.CalculateArea());
+
+            Rectangle square = new Square();
+            square.Width = 5;
+            square.Height = 4;
+            Console.WriteLine("Square Area = " + square.CalculateArea());
+        }
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,13 @@
+namespace Solution1
+{
+    public class Rectangle
+    {
+        public virtual int Height { get; set; }
+        public virtual int Width { get; set; }
+
+        public int CalculateArea()
+        {
+                return Height * Width;
+        }
+    }
+}
